485f271160cc8f601c00cd8e88455ab63a640f10
Kyle Fruson <kyle.fruson@landingzones.com>
2023-11-07 09:52:38 -0700

    Update Payload pins and mapping, add an 8th Relay while I remember how.

--- libraries/GCS_MAVLink/ap_message.h
+++ libraries/GCS_MAVLink/ap_message.h
@@ -86,6 +86,7 @@ enum ap_message : uint8_t {
     MSG_UAVIONIX_ADSB_OUT_STATUS,
     MSG_ATTITUDE_TARGET,
     MSG_HYGROMETER,
+    MSG_PAYLOAD_STATUS,
     MSG_AUTOPILOT_STATE_FOR_GIMBAL_DEVICE,
     MSG_LAST // MSG_LAST must be the last entry in this enum
 };
