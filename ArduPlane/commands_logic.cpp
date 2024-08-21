b52cfb9877dfeef42ee8204354c8fe75c0b98ab2
ayeung-superwake <alton@superwake.ca>
2024-08-21 11:10:41 -0600

    wip: debug pull up level exit logic

--- ArduPlane/commands_logic.cpp
+++ ArduPlane/commands_logic.cpp
@@ -1354,6 +1354,12 @@ bool Plane::verify_pullup(const AP_Mission::Mission_Command &cmd)
         const float pitch_lag_time = 1.0f * sqrtf(ahrs.get_EAS2TAS());
         float aspeed;
         const float aspeed_derivative = (ahrs.get_accel().x + GRAVITY_MSS * ahrs.get_DCM_rotation_body_to_ned().c.x) / ahrs.get_EAS2TAS();
+
+        float aspeed_pred = aspeed + aspeed_derivative * pitch_lag_time;
+        float aspeed_target = cmd.content.user_command.param3;
+        // print aspeed_pred, aspeed_target in GCS for debugging
+        gcs().send_text(MAV_SEVERITY_INFO, "Pullup aspeed_pred %.1f aspeed_target %.1f", aspeed_pred, aspeed_target);
+
         bool airspeed_low = ahrs.airspeed_estimate(aspeed) ? (aspeed + aspeed_derivative * pitch_lag_time) < (float)cmd.content.user_command.param3 : true;
         bool roll_control_lost = labs(ahrs.roll_sensor) > aparm.roll_limit*100;
         if (pitchup_complete && airspeed_low && !roll_control_lost) {
